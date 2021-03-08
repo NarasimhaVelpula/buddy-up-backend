149b0568c79fb2d47f11d87494c5a610402526b5
Narasimha <naninarasimha@gmail.com>
2021-03-08 11:30:25 +0530

    version 0.3

--- Server.js
+++ Server.js
@@ -1,10 +1,11 @@
 const PORT = process.env.PORT || 5000
-console.log("port")
+console.log(PORT)
 const io=require('socket.io')(PORT, {
     cors: {
       origin: '*',
     }})
 io.on('connection',socket=>{
+    console.log("connected")
     const id=socket.handshake.query.id
     socket.join(id)
     socket.on('send-message',({recipients,text})=>{
